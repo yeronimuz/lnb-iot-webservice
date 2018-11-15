b4b6b8cfca75af8d75f691ea12ac41c95d54dbb7
Jeroen Lankheet <Jeroen.Lankheet@nexus-nederland.nl>
2018-11-15 21:53:31 +0100

    remove license in every file, add generic one

--- src/main/java/com/lankheet/iot/webservice/auth/DomoticsUserAuthenticator.java
+++ src/main/java/com/lankheet/iot/webservice/auth/DomoticsUserAuthenticator.java
@@ -1,24 +1,3 @@
-/**
- * MIT License
- * 
- * Copyright (c) 2017 Lankheet Software and System Solutions
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package com.lankheet.iot.webservice.auth;
 
 import java.util.Optional;
@@ -33,7 +12,7 @@ import io.dropwizard.auth.basic.BasicCredentials;
  */
 public class DomoticsUserAuthenticator implements Authenticator<BasicCredentials, DomoticsUser> {
     @Override
-    public Optional<DomoticsUser> authenticate(BasicCredentials credentials) throws AuthenticationException {
+    public Optional<DomoticsUser> authenticate(BasicCredentials credentials) {
         if ("secret".equals(credentials.getPassword()) && "userName".equals(credentials.getUsername())) {
             return Optional.of(new DomoticsUser(credentials.getUsername(), credentials.getPassword()));
         }
